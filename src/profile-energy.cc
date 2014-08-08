34009409c7f53bebb11b1b3b40a773d5bba8ad7b
Filip Nyback <filip.nyback@fillexen.fi>
2014-08-08 14:19:59 +0300

    Clean up code.

--- src/profile-energy.cc
+++ src/profile-energy.cc
@@ -67,7 +67,7 @@ energyInit(double &scaleFactor)
     fprintf(stderr, "PAPI library initialisation failed.\n");
     return false;
   }
-  
+
   // Find the RAPL component of PAPI.
   int num_components = PAPI_num_components();
   int component_id;
@@ -85,14 +85,14 @@ energyInit(double &scaleFactor)
     fprintf(stderr, "No RAPL component found in PAPI library.\n");
     return false;
   }
-  
+
   if (component_info->disabled)
   {
-    fprintf(stderr, "RAPL component of PAPI disabled: %s.\n", 
+    fprintf(stderr, "RAPL component of PAPI disabled: %s.\n",
       component_info->disabled_reason);
     return false;
   }
-  
+
   // Create an event set.
   s_event_set = PAPI_NULL;
   if (PAPI_create_eventset(&s_event_set) != PAPI_OK)
@@ -100,7 +100,7 @@ energyInit(double &scaleFactor)
     fprintf(stderr, "Could not create PAPI event set.\n");
     return false;
   }
-  
+
   int code = PAPI_NATIVE_MASK;
   for (int retval = PAPI_enum_cmp_event(&code, PAPI_ENUM_FIRST, component_id);
        retval == PAPI_OK;
@@ -112,7 +112,7 @@ energyInit(double &scaleFactor)
       fprintf(stderr, "Could not get PAPI event name.\n");
       return false;
     }
-    
+
     PAPI_event_info_t event_info;
     if (PAPI_get_event_info(code, &event_info) != PAPI_OK)
     {
@@ -121,7 +121,7 @@ energyInit(double &scaleFactor)
     }
     if (event_info.data_type != PAPI_DATATYPE_UINT64)
       continue;
-      
+
     IgProfTrace::CounterDef *counter = 0;
     if (strstr(event_name, "PACKAGE_ENERGY_CNT"))
       counter = &s_ct_pkg;
@@ -134,7 +134,7 @@ energyInit(double &scaleFactor)
 
     if (! counter)
       continue;
-    
+
     igprof_debug("Adding %s to event set.\n", event_name);
     if (PAPI_add_event(s_event_set, code) != PAPI_OK)
       break;
@@ -146,8 +146,8 @@ energyInit(double &scaleFactor)
     fprintf(stderr, "Could not find any RAPL events.");
     return false;
   }
-  
-  // Allocate two arrays for the readings in one go: one for the current 
+
+  // Allocate two arrays for the readings in one go: one for the current
   // readings and one for the previous readings.
   s_values[0] = (long long *)calloc(2 * s_num_events, sizeof(long long));
   s_values[1] = s_values[0] + s_num_events;
@@ -156,9 +156,9 @@ energyInit(double &scaleFactor)
     fprintf(stderr, "Could not allocate memory.\n");
     return false;
   }
-  
+
   // Set the scale factor for the RAPL energy readings:
-  // one integer step is 15.3 microjoules, scale everything to millijoule.
+  // one integer step is 15.3 microjoules, scale everything to joules.
   scaleFactor = 0.0000153;
 
   return true;
@@ -178,7 +178,7 @@ energyStart(void)
 #ifdef PAPI_FOUND
   if (PAPI_start(s_event_set) == PAPI_OK)
     return true;
-  
+
   fprintf(stderr, "Could not start measuring energy using PAPI.\n");
 #endif
   return false;
@@ -338,20 +338,8 @@ initialize(void)
   if (! enable)
     return;
 
-/*
-  double clockres = 0;
-  itimerval precision;
-  itimerval interval = { { 0, 5000 }, { 100, 0 } };
-  itimerval nullified = { { 0, 0 }, { 0, 0 } };
-  setitimer(s_itimer, &interval, 0);
-  getitimer(s_itimer, &precision);
-  setitimer(s_itimer, &nullified, 0);
-  clockres = precision.it_interval.tv_sec
-             + 1e-6 * precision.it_interval.tv_usec;
-*/
-
   // Set up PAPI for energy measurements.
-  double scaleFactor; 
+  double scaleFactor;
   if (! energyInit(scaleFactor))
     _exit(1);
 
@@ -379,7 +367,7 @@ initialize(void)
   // Start measuring energy.
   if (! energyStart())
     _exit(1);
-  
+
   igprof_debug("energy profiler enabled\n");
 
   enableSignalHandler();
